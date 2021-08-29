ac455750b25d008d5695fa0762bf2383e6a63188
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-08-28 18:06:20 -0700

    SR: list with links to codepen

---
+++ public/assets/js/script.js
@@ -0,0 +1,9 @@
+// ~~~~~~~~~~~~ Hilfs-Funktion ~~~~~~~~~~~ //
+function getEl(para1){
+    var output = document.getElementById(para1);
+    return output;
+};
+
+// ~~~~~~~~~~~~~~~~~~ JS ~~~~~~~~~~~~~~~~~ // 
+
+console.log('test');
